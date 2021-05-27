7ac8d18f00afa45d15b52a0edfc0bc3fcae51e3f
victorsilvajc021 <victorsilvajc@hotmail.com>
2021-05-27 15:43:04 -0300

    Atualizando brach

--- Queries/respostas_brainstorm_victor.sql
+++ Queries/respostas_brainstorm_victor.sql
@@ -16,10 +16,22 @@ group by
     d.surname
 order by
     count (r.driverid) desc
+
+
 --2: Qual piloto teve a volta mais rápida?
+
+select 
+	d.surname as piloto,
+	r.fastestlaptime as tempo,
+	r.milliseconds as milisegundos
+from 
+	results r 
+	join drivers d on r.driverid = d.driverid
+	where fastestlaptime = (select min(fastestlaptime) from results);
 
 
 --3: Qual escuderia teve mais pontos?
+
 select
     c.name,
     sum(cs.points) as total_pontos
@@ -30,7 +42,9 @@ group by
     c.name
 order by
     total_pontos desc
+
 --4: Qual país tem mais vitórias?
+
 select
 	d.nationality,
 	SUM (r."position") as total_vitorias
@@ -45,7 +59,21 @@ order by total_vitorias desc
 
 --5: Qual a pista mais rápida?
 
+select  
+	r2.name as pista,
+	d.surname as piloto,
+	r.fastestlaptime as menor_tempo
+from results r
+	join races r2 on r.raceid = r2.raceid
+	join drivers d on d.driverid = r.driverid
+	where fastestlaptime = (select min(fastestlaptime) from results);
+
 --6: Qual é a média de pit stops?
+
+select 
+	avg(duration) as media_pit_stop 
+from 
+	pitstops;
 
 --7: Quantos brasileiros foram campeões?
     select
@@ -62,6 +90,21 @@ group BY
 order by vitorias desc;
 
 --8: Qual piloto teve mais pole position?
+
+select 
+	d.forename,
+	d.surname,
+	d.driverid,
+	sum(case when q."position"=1 then 1 else 0 end) as total_pole_positions
+from qualifying q
+	join drivers d on d.driverid=q.driverid
+group by
+	d.forename,
+	d.surname,
+	d.driverid
+order by
+	total_pole_positions desc
+	limit 1;
 
 --9: Pegar o melhor desempenho na corrida, os pit stops afetaram?
 
@@ -81,11 +124,33 @@ order by vitorias desc;
 
 --17: Qual construtor e motorista relacionado mais se destacaram dentre as 5 primeiras posições durante os campeonatos?
 
---18: De quais países advém a maior parte dos construtores?
+--18: Quais países que advém a maior parte das escuderias?
+
+select 
+	nationality as nacionalidade,
+	count(nationality) as Numero_escuderias
+from 
+	constructors
+group by 
+	nationality
+order by 
+	Numero_escuderias desc
+	limit 3;
  
 --19: Quais as nacionalidades presentes nos campeonatos? e porque de british? - Será que há algum incentivo diferenciado?
  
 --20: Construtora que obteve mais pontos durante o período analisado...
+
+select 
+	c2.name, 
+	count(points) as pontos
+from constructorstandings c1
+	join constructors c2 on c1.constructorid = c2.constructorid
+group by 
+	name
+order by 
+	pontos desc 
+	limit 1;
  
 --21: Quais os pit-stops mais rápidos dentre os anos analisados e de quem foi? - parada do carro para troca de pneus e reabastecimento
 
@@ -94,8 +159,6 @@ order by vitorias desc;
 --23: Qual construtora se destacou com os menores pit_stops durante o espaço de tempo analisado?
 
 --24: Nome do circuito mais presente em cada ano analisado e a quantidade de vezes que aparece.
-
-select * from races;
 
 --Campeoes por ano (mais ou menos):
 
